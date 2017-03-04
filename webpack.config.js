9341445290451e25131bcbbdcf16e6c4de75a22e
Humniba <humniba@outlook.com>
2017-03-05 01:21:12 +0800

    update 引入 vue

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,8 @@
-/**
- * Build webpack configuration
- * @type {[type]}
- */
-const path = require('path'),
-      args = require('minimist')(process.argv.slice(2));
+var path = require('path');
 
-// List of environments
-const envs = ['dev', 'dist', 'test'];
-
-// Set the environment
-let env;
-if (args._.length > 0 && args._.indexOf('start') !== -1) {
-  env = 'test';
-} else if (args.env) {
-  env = args.env;
-} else {
-  env = 'dev';
-}
-process.env.REACT_WEBPACK_ENV = env;
-
-/**
- * Build the webpack configuration
- * @param  {String} buildEnv
- * @return {Object} Webpack config
- */
-function buildConfig(buildEnv) {
-  let isValid = buildEnv && buildEnv.length > 0 && envs.indexOf(buildEnv) !== -1;
-  let validEnv = isValid ? buildEnv : 'dev';
-  let config = require(path.resolve(__dirname, 'conf/' + validEnv));
-  return config;
-}
-
-module.exports = buildConfig(env);
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'src/index.js'),
+    vendors: path.resolve(__dirname, 'src/vendors.js')
+  }
+};
